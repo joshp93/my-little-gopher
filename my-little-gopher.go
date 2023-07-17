63fc3e3039bbb80ea599fb7369e83c7677b81d67
Joshua Pearson <joshua.pearson@nationalexpress.com>
2023-07-17 12:25:45 +0100

    my-little-gopher first go app

---
+++ my-little-gopher.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+
+	"rsc.io/quote"
+)
+
+func main() {
+	fmt.Println("My little gopher, my little gopher, will always be in my heart 🦡")
+	fmt.Println(quote.Go())
+}
